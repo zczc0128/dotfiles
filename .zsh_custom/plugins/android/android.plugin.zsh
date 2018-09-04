8e82eec6feb029fceb8343678befee1ff9c5841a
zhoumingjun <zhoumingjun@gmail.com>
2018-09-04 13:51:48 +0800

    update

--- .zsh_custom/plugins/android/android.plugin.zsh
+++ .zsh_custom/plugins/android/android.plugin.zsh
@@ -2,14 +2,14 @@ androiddirs=("${HOME}/Library/Android" "${HOME}/Android" )
 
 FOUND_ANDROID=0
 for androiddir in $androiddirs; do
-    if [[ -d "${androiddir}/sdk" ]]; then
+    if [[ -d "${androiddir}/Sdk" ]]; then
         FOUND_ANDROID=1
         break
     fi
 done
 
 if [[ $FOUND_ANDROID -eq 1 ]]; then
-    export ANDROID_HOME=${androiddir}/sdk
+    export ANDROID_HOME=${androiddir}/Sdk
     export PATH=${ANDROID_HOME}/tools/bin:${ANDROID_HOME}/platform-tools:$PATH
 
 else

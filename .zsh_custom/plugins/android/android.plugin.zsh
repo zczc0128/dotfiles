aa2fbd4a8b6e629499833b6b567ad26d2775b888
zhoumingjun <zhoumingjun@gmail.com>
2018-10-25 12:56:57 +0800

    update

--- .zsh_custom/plugins/android/android.plugin.zsh
+++ .zsh_custom/plugins/android/android.plugin.zsh
@@ -10,8 +10,7 @@ done
 
 if [[ $FOUND_ANDROID -eq 1 ]]; then
     export ANDROID_HOME=${androiddir}/Sdk
-    export PATH=${ANDROID_HOME}/tools:${ANDROID_HOME}/tools/bin:${ANDROID_HOME}/platform-tools:$PATH
-
+    export PATH=${ANDROID_HOME}/emulator:${ANDROID_HOME}/tools:${ANDROID_HOME}/tools/bin:${ANDROID_HOME}/platform-tools:$PATH
 else
 fi
 
